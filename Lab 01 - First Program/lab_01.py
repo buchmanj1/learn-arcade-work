e1111c5f43795680905fea7614d523cf257e8c48
buchmanj1 <159155313+buchmanj1@users.noreply.github.com>
2024-02-08 09:35:14 -0500

    Update lab_01.py

--- Lab 01 - First Program/lab_01.py
+++ Lab 01 - First Program/lab_01.py
@@ -1,1 +1,8 @@
-Sup
+print("Hold your ground! Hold Your Ground!. \nSons of Gondor, of Rohan, my brothers.")
+print("""A day may come when the courage of men fails,
+when we forsake and break all bonds of fellowship, but it is not this day.
+An hour of wolves and shattered shields,
+when the age of men comes crashing down,
+but it is not this day!""")
+print("This day we fight!")
+print("By all that you hold dear on this good Earth, \nI bid you stand, \nMen of the West!")
