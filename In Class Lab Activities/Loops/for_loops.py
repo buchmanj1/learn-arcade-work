4c51d790f68cc8c99a690f5b44696d50cb447d5c
buchmanj1 <159155313+buchmanj1@users.noreply.github.com>
2024-02-29 09:05:18 -0500

    Update for_loops.py

--- In Class Lab Activities/Loops/for_loops.py
+++ In Class Lab Activities/Loops/for_loops.py
@@ -4,14 +4,22 @@
 
 
 
-
-
 for i in range (5):
     print("Hello")
-print("There")"""
+print("There")
+
 
 
 for i in range (5):
     print("Hello")
     print("There")
 # General Kenobi
+
+
+
+for i in range (10):
+    print(i)"""
+
+
+
+
