1fc95f1bb1614e7c0d84202d0c04ee8826eac144
buchmanj1 <159155313+buchmanj1@users.noreply.github.com>
2024-02-29 09:16:18 -0500

    Update for_loops.py

--- In Class Lab Activities/Loops/for_loops.py
+++ In Class Lab Activities/Loops/for_loops.py
@@ -18,8 +18,21 @@ for i in range (5):
 
 
 for i in range (10):
+    print(i)
+
+
+
+for i in range (1,11):
+    print(i)
+
+
+for i in range (10):
+    print(i+1)
+
+
+for i in range (2, 12, 2):
     print(i)"""
 
 
-
-
+for i in range (5):
+    print((i+1)*2)
