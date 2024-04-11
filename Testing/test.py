2c34b639047b400fcd2bc47ad627624881be027c
buchmanj1 <159155313+buchmanj1@users.noreply.github.com>
2024-04-11 09:39:00 -0400

    Update lab_04.py
    It's finally done!

--- Testing/test.py
+++ Testing/test.py
@@ -1,53 +1,9 @@
-"""class Cat:
-    def __init__(self):
-        self.color = ""
-        self.name = ""
-        self.weight = 0
+for row in range(10):
 
-    def meow(self):
-        print("Meow!")
+    for j in range(row):
+        print(" ", end=" ")
 
+    for j in range(10 - row):
+        print(j, end=" ")
 
-my_cat = Cat()
-my_cat.color = "Brown"
-my_cat.name = "Tank"
-my_cat.weight = 10
-
-print("My cats name is", my_cat.name)
-my_cat.meow()
-
-class Monster:
-    def __init__(self):
-        self.name = ""
-        self.health = 0
-
-    def decrease_health(self, amount):
-        self.health -= amount
-        if 0 >= self.health:
-            print("The monster is dead.")
-
-
-monster1 = Monster()
-monster1.name = "Omnom"
-monster1.health = 30
-
-print("The monster is", monster1.name)
-monster1.decrease_health(31)
-
-class Monster:
-    def __init__(self, health, name):
-        self.name = name
-        self.health = health
-
-    def decrease_health(self, amount):
-        self.health -= amount
-        if 0 >= self.health:
-            print("The monster is dead.")
-
-class Star:
-    def __init__(self):
-        print("A star is born!")
-
-star1 = Star()
-star2 = Star()
-star3 = Star()"""
+    print()
